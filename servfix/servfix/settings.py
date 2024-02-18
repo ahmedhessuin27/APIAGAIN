dab1f86817e442db0f1e1709353387fe7eb04e60
ahmedhessuin27 <am8150688@gmail.com>
2024-02-18 21:46:02 +0200

    version

--- servfix/servfix/settings.py
+++ servfix/servfix/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 # ALLOWED_HOSTS = ['localhost']  
 # localhost for delete
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -166,10 +166,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
                   
 ]
 
